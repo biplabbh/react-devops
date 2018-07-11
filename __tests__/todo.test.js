463e3d0e37ecc546fbea4f102b9799d62727ceee
Bhattacharya <Biplab.Bhattacharya@absa.co.za>
2018-07-11 17:36:09 +0200

    First commit

---
+++ __tests__/todo.test.js
@@ -0,0 +1,9 @@
+import Todo from '../app/todo';
+import React from 'react';
+import { mount } from 'enzyme';
+
+describe('App', () => {
+    it('should be able to run tests', () => {
+        expect(1 + 2).toEqual(3);
+    });
+});
